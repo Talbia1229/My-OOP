b8546f9d52d22f2b9fd5cacda60601030fc963d4
Talbia Razzaq <talbia1229@gmail.com>
2025-09-17 12:53:37 +0500

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,12 @@
+// Talbia Razzaq(SP25-BCS-154) Task 1
+public class Student{
+    private String name;
+	private String email;
+	private double cgpa;
+		
+	public void display(){
+	   System.out.println(name);
+	   System.out.println(email);
+	   System.out.println(cgpa);
+	}
+}
---
+++ StudentTest.java
@@ -0,0 +1,7 @@
+// Talbia Razzaq(SP25-BCS-154) TASK 1
+class StudentTest{
+    public static void main(String args[]){
+		Student std = new Student();
+		std.display();
+	}
+}
